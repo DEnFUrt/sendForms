d59a67c95a40863f7d3d8292833092a3869da29f
Denis <57797457+DEnFUrt@users.noreply.github.com>
2020-03-14 11:45:57 +0500

    Added by Promise

--- sendForms.js
+++ sendForms.js
@@ -4,65 +4,76 @@ const message = {
     failure: 'Ошибка отправки, отправьте сообщение через несколько минут.',
 }
 
+const postData = (objFormData) => {
+    return new Promise( (resolve, reject) => {
+        let request = new XMLHttpRequest();
+        
+        request.open('POST', 'https://my-json-server.typicode.com/DEnFUrt/json-repo/posts');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        
+        request.upload.onprogress = () => {
+            resolve();
+        };
+        
+        request.onload = () => {
+            if (request.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+                reject(new Error(`Ошибка ${request.status}: ${request.statusText}`));
+            } else { 
+                resolve();
+            }
+        };
+          
+        request.onerror = () => { // происходит, только когда запрос совсем не получилось выполнить
+            reject(new Error(message.failure));
+        };
+
+        request.send(JSON.stringify(objFormData));
+    });
+};
+
 export function sendForms(idForm) {   
-    let alertMessage = this.querySelector('div.status'); //статус отправки выводится в div который создается в момент отправки
-    if (!alertMessage) {                                 //можно реализовать свой способ оповещения пользователя
+    let alertMessage = this.querySelector('div.status');
+    
+    if (!alertMessage) {
         alertMessage = document.createElement('div');
         alertMessage.classList.add('status');
         this.append(alertMessage);
     } else {
         alertMessage.textContent = '';
-    }   
-        
-    let request = new XMLHttpRequest();
-
-    request.open('POST', 'https://my-json-server.typicode.com/DEnFUrt/json-repo/posts');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
+    }  
+    
     let formData = new FormData(this);
 
     formData.append('theme', idForm);
-    let obj = {};
+    
+    let objFormData = {};
     formData.forEach( (item, key) => {
-        obj[key] = item;
+        objFormData[key] = item;
     });
-
-    request.send(JSON.stringify(obj));
     
-    request.upload.onprogress = () => {
-        alertMessage.textContent = message.loading;
-    };
-    
-    request.onload = () => {
-        if (request.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-            alertMessage.textContent = `Ошибка ${request.status}: ${request.statusText}`; // Например, 404: Not Found
-        } else { 
+    postData(objFormData)
+        .then( () => alertMessage.textContent = message.loading)
+        .then( () => {
             alertMessage.textContent = message.success;
-            //console.log(request.response);
             this.reset();
-        }
-    };
-      
-    request.onerror = () => { // происходит, только когда запрос совсем не получилось выполнить
-        alertMessage.textContent = message.failure;
-    };
-    
+        })
+        .catch(error => alertMessage.textContent = error.message);
+
+
     //вариант с событиями readystatechange
     /* request.addEventListener('readystatechange', () => {
         switch (true) {
             case request.readyState < 4 :
-                alertMessage.textContent = message.loading;
+                resolve();
                 break;
             case request.readyState === 4 :
-                alertMessage.textContent = message.success;
+                resolve();
                 //console.log(request.response);
-                this.reset();
-                break;
             default:
                 alertMessage.textContent = message.failure;
+                reject(new Error(`Ошибка ${request.status}: ${request.statusText}`));
                 //console.log(request.response);
                 break;
         }
     }); */
 };
-
